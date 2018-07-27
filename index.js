bb533159dea89e3866180d125a842c1e92742cc9
Cris Hanks <crishanks12@gmail.com>
2018-07-27 00:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
-
-function holidayCountdown(holiday, days){
-  return `It's ${days} days until ${holiday}!`
-}
